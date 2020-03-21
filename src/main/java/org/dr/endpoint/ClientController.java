b57d91c2d006ec8043b47271f2efc6b6afbd84f0
Dominik Rafacz <d.rafacz@student.mini.pw.edu.pl>
2020-03-21 23:54:12 +0100

    change of security keys and client controller

--- src/main/java/org/dr/endpoint/ClientController.java
+++ src/main/java/org/dr/endpoint/ClientController.java
@@ -1,15 +1,46 @@
 package org.dr.endpoint;
 
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@Profile("server")
+import javax.net.ssl.SSLContext;
+import java.util.Collections;
+
+@Profile("client")
 @RestController
 public class ClientController {
 
+    private static final String OUTER_URL = "https://192.168.1.100/shelter";
+
+    @Value("${trust.store}")
+    private Resource trustStore;
+
+    @Value("${trust.store.password}")
+    private String trustStorePassword;
+
     @RequestMapping("/inside")
-    public String inside(){
-        return null;
+    public String inside() throws Exception {
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+                .build();
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(socketFactory)
+                .build();
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        String tmp = new RestTemplate(factory).getForEntity(OUTER_URL, String.class, Collections.emptyMap()).getBody();
+        System.out.println(tmp);
+        return tmp;
     }
 }
